3d3eeba46b39c8045932c112653aa186ff523c68
Sanjeet <sanjeet.kaur849@gmail.com>
2019-07-28 10:25:15 +0530

    Committing First java code

---
+++ BasicCoreJava/src/variablesandatatype/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package variablesandatatype; //package info where it is reserved
+
+public class FirstJavaClass { //class inside package
+
+	public static void main(String[] args) 
+	{ //must have 'main function' in ur class, static: nature of functions, void:return type
+		// TODO Auto-generated method stub
+		System.out.println("First selenium class"); //class name always starts with caps, variable small.
+
+	}
+
+}

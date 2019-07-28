210fa6e97f1fc21007ba6d4a3da3b8b1a918503e
Sanjeet <sanjeet.kaur849@gmail.com>
2019-07-28 11:08:15 +0530

    CommitingDatatypesOfjava

---
+++ BasicCoreJava/src/variablesandatatype/SecondJavaClass.java
@@ -0,0 +1,24 @@
+package variablesandatatype;
+
+public class SecondJavaClass {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		//Arithmetic operators
+		
+	int a=10;
+	int b=20;
+	
+	//+, -,*, /, %
+	
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a%b);
+
+
+	}
+
+}

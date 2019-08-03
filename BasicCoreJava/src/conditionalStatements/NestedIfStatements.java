a5ea00f377a64a3019ca4dc90c28b1e3ec2ec7a4
Sanjeet <sanjeet.kaur849@gmail.com>
2019-08-03 12:32:08 +0530

    comitting

---
+++ BasicCoreJava/src/conditionalStatements/NestedIfStatements.java
@@ -0,0 +1,37 @@
+package conditionalStatements;
+
+public class NestedIfStatements 
+{
+
+	public static void main(String[] args) 
+	
+	{
+		int a=10;
+	    int b=20;
+		int c=30;
+		
+		
+		if (a>b)
+		{
+			
+			if(a>c)
+			{
+				System.out.println("a is greatest");
+			}
+			else
+			{
+				System.out.println("a is greater than b but less than c");
+			}/
+			
+		}
+		else
+		{
+			System.out.println("a is less than b");
+		}
+			
+			
+		
+
+	}
+
+}
